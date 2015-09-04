36a3a47e1bf08a41a8c2727f7bce4eb2675a7fa5
Angel Santiago Jaime Zavala <anhell.death999@gmail.com>
2015-09-04 13:50:04 -0500

    Some extra changes to folders

--- Algorithms/Sorting/MergeSort.cpp
+++ Algorithms/Sorting/MergeSort.cpp
@@ -1,22 +1,5 @@
-#include <iostream>
 #include <cstdio>
-#include <cstring>
-#include <string>
-#include <algorithm>
-#include <cmath>
-#include <vector>
-#include <set>
-#include <map>
-#include <cstdlib>
-#include <cctype>
-#include <bitset>
-#include <sstream>
-#include <stack>
 #include <queue>
-#include <deque>
-#include <list>
-#include <iomanip>
-#include <utility>
 using namespace std;
 
 void merge(int s[], int low, int middle, int high) {
